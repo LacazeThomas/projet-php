3c87d80c3bfe808f4b3adb6f37d85c3b2a86f6b7
Thomas LACAZE <contact@thomaslacaze.fr>
2018-11-11 14:25:04 +0100

    Bug pdf 2

--- admin/pdf/index.php
+++ admin/pdf/index.php
@@ -2,26 +2,6 @@
 session_start();
 require('mc_table.php');
 
-function GenererMot()
-{
-    //Renvoie un mot aléatoire
-    $nb=rand(3,10);
-    $mot='';
-    for($i=1;$i<=$nb;$i++)
-        $mot.=chr(rand(ord('a'),ord('z')));
-    return $mot;
-}
-
-function GenererPhrase()
-{
-    //Renvoie une phrase aléatoire
-    $nb=rand(1,10);
-    $p='';
-    for($i=1;$i<=$nb;$i++)
-        $p.=GenererMot().' ';
-    return substr($p,0,-1);
-}
-
 $pdf=new PDF_MC_Table();
 $pdf->AddPage();
 $pdf->SetFont('Arial','',11);
