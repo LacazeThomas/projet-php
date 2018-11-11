9e53fdc30dac21aa77a663d22ee1ba387c027ee3
StevenK8 <43873889+StevenK8@users.noreply.github.com>
2018-11-11 13:17:57 +0100

    Style CSS
    Correction de l'affichage et ajout d'un style CSS

--- edt/index.php
+++ edt/index.php
@@ -1,12 +1,13 @@
-<style>
-form{
-	display: table;
-}
-</style>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
 <?php
 session_start();
 
 $tabue = array("Mathématiques","Anglais","Programmation","Algorithmique","Economie");
+$tabnote = array("Très mécontent","Mécontent","Moyen","Satisfait","Très satisfait");
 
 if ($_SESSION["role"] == "edt"){
     echo $_SESSION["id"];
@@ -14,12 +15,16 @@ if ($_SESSION["role"] == "edt"){
 	if (file_exists ($vote)){ //Si l'étudiant a voté
 		$row = 1;
 		if (($handle = fopen($vote, "r")) !== FALSE) {
-			while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-				$num = count($data);
-				echo "<p> Vous avez déjà voté : <br /></p>\n";
-				$row++;
-				for ($c=0; $c < $num; $c++) {
-					echo $tabue[$c] . " : " . $data[$c] . "<br />\n";
+			while (($note = fgetcsv($handle, 1000, ",")) !== FALSE) {
+				$num = count($note);
+				if($num == 5){
+					echo "<h3> Vous avez déjà voté : <br /></h3>\n";
+					$row++;
+					echo "<table>";
+					for ($c=0; $c < $num; $c++) {
+						echo "<tr><td><strong>" . $tabue[$c] . "</strong></td><td>" . $tabnote[$c] . "</td></tr>";
+					}
+					echo "</table>";
 				}
 			}
 			fclose($handle);
@@ -28,22 +33,23 @@ if ($_SESSION["role"] == "edt"){
 		print('
 		<h2 class="title">Formulaire de vote</h2>
 		<form method="POST" action="writevote.php">
-			<fieldset>');
+			<table>');
 			foreach ($tabue as $ue){
-				echo $ue . " : \t" ;
-				echo "<select name=" . $ue . ">";
+				
+				echo "<tr><td>" . $ue . "</td>" ;
+				echo "<td><select name=" . $ue . " class = 'chosen-value'>";
 				print('
 					<option value="1">Très mécontent</option>
 					<option value="2">Mécontent</option>
 					<option value="3" selected>Moyen</option>
 					<option value="4">Satisfait</option>
 					<option value="5">Très satisfait</option>
-				</select>
-				</br>');
+				</select></td>
+				</tr>');
 			}
 		print('
-				<button type="submit">Valider</button>
-			</fieldset>
+				<tr><td colspan="2"><center><button type="submit">Valider</button></center></tr></td>
+			</table>
 		</form>');
 	}
 	
@@ -51,8 +57,9 @@ if ($_SESSION["role"] == "edt"){
     header('Location: login.php');  
 }
 ?>
-
+</body>
+</html>
 
 <form action="../logout.php" method="post">
- <p><input type="submit" name="submit" value="Déconnexion"></p>
+ <p><button type="submit" name="submit" value="Déconnexion">Déconnexion</button></p>
 </form>
