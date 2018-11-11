60de1c5bded401b7e77fd32bd750bb82e55953bc
StevenK8 <43873889+StevenK8@users.noreply.github.com>
2018-11-11 14:36:21 +0100

    Ajout de la feuille de style

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
-Vous êtes: 
+<html>
+<head>
+<link rel="stylesheet" href="assets/css/style.css" />
+</head>
+<body>
+Vous êtes:</br>
 
 <a href="admin/login.php">Administrateur</a>
 <a href="edt/login.php">Étudiant</a>
 <a href="prof/login.php">Professeur</a>
+</body>
+</html>
