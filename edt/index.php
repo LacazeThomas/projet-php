212836274ea8e32bab7d14beba2ccbb67829060a
Thomas LACAZE <contact@thomaslacaze.fr>
2018-11-19 20:33:00 +0100

    Restructuration page prof

--- edt/index.php
+++ edt/index.php
@@ -1,38 +1,46 @@
 <?php
 require_once '../panel_header.php';
 
-$tabue = array("Mathématiques","Anglais","Programmation","Algorithmique","Economie");
-$tabnote = array("Très mécontent","Mécontent","Moyen","Satisfait","Très satisfait");
 
-if ($_SESSION["role"] == "edt"){
-	$vote = "votes/vote-".$_SESSION["id"].".csv";
-	if (file_exists ($vote)){ //Si l'étudiant a voté
-		$row = 1;
-		if (($handle = fopen($vote, "r")) !== FALSE) {
-			while (($note = fgetcsv($handle, 1000, ",")) !== FALSE) {
-				$num = count($note);
-				if($num == 5){
-					echo "<h3> Vous avez déjà voté :</h3>\n";
-					$row++;
-					echo "<table class='table'><tbody>";
-					for ($c=0; $c < $num; $c++) {
-						echo "<tr><td><strong>" . $tabue[$c] . "</strong></td><td>" . $tabnote[$note[$c]-1] . "</td></tr>";
-					}
-					echo "<tbody></table>";
-				}
-			}
-			fclose($handle);
-		}
-	}else{
-		print('
+
+$tabue = array("Mathématiques", "Anglais", "Programmation", "Algorithmique", "Economie");
+$tabnote = array("Très mécontent", "Mécontent", "Moyen", "Satisfait", "Très satisfait");
+if ($_SESSION["role"] == "edt") {
+	if (isset($_SESSION["error"])) {
+		echo "<div class=\"alert alert-danger\" role=\"alert\">
+			" . $_SESSION["error"] . "
+			</div>";
+		unset($_SESSION["error"]);
+	}
+	
+    $vote = "votes/vote-" . $_SESSION["id"] . ".csv";
+    if (file_exists($vote)) { //Si l'étudiant a voté
+        $row = 1;
+        if (($handle = fopen($vote, "r")) !== false) {
+            while (($note = fgetcsv($handle, 1000, ",")) !== false) {
+                $num = count($note);
+                if ($num == 5) {
+                    echo "<h3> Vous avez déjà voté :</h3>\n";
+                    $row++;
+                    echo "<table class='table'><tbody>";
+                    for ($c = 0; $c < $num; $c++) {
+                        echo "<tr><td><strong>" . $tabue[$c] . "</strong></td><td>" . $tabnote[$note[$c] - 1] . "</td></tr>";
+                    }
+                    echo "<tbody></table>";
+                }
+            }
+            fclose($handle);
+        }
+    } else {
+        print('
 		<h2 class="title">Formulaire de vote</h2>
 		<form method="POST" action="writevote.php">
 			<table class="table"><tbody>');
-			foreach ($tabue as $ue){
-				
-				echo "<tr><th scope='row'>" . $ue . "</th>" ;
-				echo "<td><select name=" . $ue . " class = 'chosen-value'>";
-				print('
+        foreach ($tabue as $ue) {
+
+            echo "<tr><th scope='row'>" . $ue . "</th>";
+            echo "<td><select name=" . $ue . " class = 'chosen-value'>";
+            print('
 					<option value="1">Très mécontent</option>
 					<option value="2">Mécontent</option>
 					<option value="3" selected>Moyen</option>
@@ -40,21 +48,16 @@ if ($_SESSION["role"] == "edt"){
 					<option value="5">Très satisfait</option>
 				</select></td>
 				</tr>');
-			}
-		print('
+        }
+        print('
 				</tbody><tr><td colspan="2"><center><button class="btn btn-primary" type="submit">Valider</button></center></tr></td>
 			</table>
 		</form>');
-	}
-	if (isset($_SESSION["error"])){
-		echo "<p style='color:Tomato;font-weight: bold;'>---------------- ".htmlspecialchars($_SESSION["error"])." ----------------</p>";
-		unset ($_SESSION["error"]);
-	}
-	
-}else{
-    header('Location: ../index.php');  
+    }
+
+} else {
+    header('Location: ../index.php');
 }
 ?>
 <?php
 require_once '../panel_footer.php';
-?>
--- prof/index.php
+++ prof/index.php
@@ -23,7 +23,7 @@ if ($_SESSION["role"] == "prof") {
     }
     echo "<h2>Voici les avis des etudiants pour la matière ";
     $prof_ue = (int) filter_var($_SESSION["id"], FILTER_SANITIZE_NUMBER_INT);
-    switch ($prof_ue){
+    switch ($prof_ue) {
         case 1:
             echo "Mathématiques ";
             break;
@@ -41,27 +41,33 @@ if ($_SESSION["role"] == "prof") {
             break;
     }
     echo "sont: </h2>";
-    
-    foreach ($avis as $avi) {
-        switch ($avi[$prof_ue-1]){
-            case 1:
-                echo "Très mécontent </br>";
-                break;
-            case 2:
-                echo "Mécontent </br>";
-                break;
-            case 3:
-                echo "Moyen </br>";
-                break;
-            case 4:
-                echo "Satisfait </br>";
-                break;
-            case 5:
-                echo "Très satisfait </br>";
-                break;
-        }
+
+    $notation = array("", "Très mécontent", "Mécontent", "Moyen", "Satisfait", "Très satisfait");
+    echo "<div class=\"table-responsive-sm\"><table class=\"table\"><thead><tr>";
+
+    foreach ($notation as $avi) {
+        echo "<th>" . $avi . "</th>";
     }
 
+    echo "</tr></thead>";
+    echo "<tbody>";
+    $nombre_votant = count($votes)-1;
+    echo "<tr><th scope=\"row\">Répartion</th>";
+        for ($i = 0; $i < 5; $i++) {
+            $count = 0;
+            for ($y = 0; $y < $nombre_votant ; $y++) {
+                if ($avis[$y][$prof_ue - 1] == $i + 1) {
+                    $count++;
+                }
+            }
+        echo "<td>" . $count . "</td>";
+    }
+    echo "</tbody></table></div>";
+    if($nombre_votant > 1){
+        echo "<h3>Il y a ".$nombre_votant." étudiants qui ont voté </h3>";
+    }else{
+        echo "<h3>Il y a 1 étudiant qui a voté </h3>";
+    }
 } else {
     header('Location: ../');
 }
