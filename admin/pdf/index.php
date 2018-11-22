eec2924ba4febd1c40c3532048f0407e87749005
Aurelien Brunet <32396173+Mem2o@users.noreply.github.com>
2018-11-22 16:39:27 +0100

    Add files via upload

--- admin/pdf/index.php
+++ admin/pdf/index.php
@@ -3,14 +3,17 @@ session_start();
 require('mc_table.php');
 
 $pdf=new PDF_MC_Table();
+$pdf->AliasNbPages(); // affiche le nombre de page sous le "/"
 $pdf->AddPage();
 $pdf->SetFont('Arial','',11);
 //Table de 20 lignes et 4 colonnes
 $pdf->SetWidths(array(23,23,23,23,23,23,23,23));
-srand(microtime()*1000000);
+
+//srand(microtime()*1000000);
+
 foreach($_SESSION["table"] as $row){
     $pdf->Row($row);
 }
-    
+
 $pdf->Output();
 ?>
