d69d90fcaafa14ff0943ca6de5451c7ad8b8880e
Thomas LACAZE <contact@thomaslacaze.fr>
2018-11-24 22:46:32 +0100

    Modification pdf couleur pdf

--- admin/pdf/index.php
+++ admin/pdf/index.php
@@ -1,30 +1,25 @@
 <?php
 session_start();
-require('mc_table.php');
+require 'mc_table.php';
 
-$pdf=new PDF_MC_Table();
+$pdf = new PDF_MC_Table();
 $pdf->AliasNbPages(); // affiche le nombre de page sous le "/"
 $pdf->AddPage();
-$pdf->SetFont('Arial','',11);
+$pdf->SetFont('Arial', '', 11);
 //Table de 20 lignes et 4 colonnes
-$pdf->SetWidths(array(23,23,23,23,23,23,23,23));
+$pdf->SetWidths(array(29, 35, 24, 16, 17, 26, 18, 20));
 
-//srand(microtime()*1000000);
-$compteur = 1;
-foreach($_SESSION["table"] as $row){
-	
-	if ($compteur == 1)
-	{
-	$pdf->SetFillColor('144', '146', '150');
-	$pdf->Row2($row);
-	$compteur = $compteur + 1;
-	}
-	else
-	{
-	$pdf->Row($row);
-	$compteur = $compteur + 1;
-	}
+srand(microtime() * 1000000);
+$first_line = true;
+foreach ($_SESSION["table"] as $row) {
+
+    if ($first_line == true) {
+        $pdf->SetFillColor('144', '146', '150');
+		$pdf->Row2($row);
+        $first_line = false;
+    } else {
+        $pdf->Row($row);
+    }
 }
 
 $pdf->Output();
-?>
