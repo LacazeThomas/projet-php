24c9d3841679004bc59a8d1061bad7f5c59822d7
Thomas LACAZE <contact@thomaslacaze.fr>
2018-11-24 12:04:48 +0100

    Ajout de couleur pdf et footer (integration)

--- admin/pdf/mc_table.php
+++ admin/pdf/mc_table.php
@@ -1,26 +1,30 @@
 <?php
 require('fpdf181/fpdf.php');
-
 class PDF_MC_Table extends FPDF
 {
 var $widths;
 var $aligns;
-
 function Header (){
 			$this->SetFont("Arial","b",10);
+			
+			$this->SetFillColor(0,0,255);
 			$this->Image('uvsq.jpg',162,5,35);
 			$this->Cell(2,10,date("d/m/Y"),0,0);
 			$this->Cell(100,10,'',0,1); 
 			$this->Cell(100,10,'',0,1); 
-			$this->Cell(185,10,'Tableau recapitulatif de l\'avis des '.utf8_decode(é).'tudiants',1,1,'C');
+			$this->Cell(185,10,'Tableau recapitulatif de l\'avis des etudiants',1,1,'C');
 			$this->Cell(100,10,'',0,1); 
-
 		}
 		
 function Footer (){
 			$this->SetFont("Arial","b",10);
 			$this->SetY(270);
+			$fill = 1;
+			
 			$this->Cell(0,10,'Page '.$this->PageNo().'/{nb}','B',0,'C');
+			$this->SetFont("Arial","",7);
+			$this->Write(10, 'Thomas Lacaze, Jibril Zioui, Aurelien Brunet, Steven Kerautret, Noe Meric de Bellefon, Benjamin Ngogo, Projet PHP 2018 ');
+
 			
 			
 		}
@@ -29,16 +33,15 @@ function SetWidths($w)
 	//Tableau des largeurs de colonnes
 	$this->widths=$w;
 }
-
 function SetAligns($a)
 {
 	//Tableau des alignements de colonnes
 	$this->aligns=$a;
 }
-
-function Row($data)
+function Row2($data)
 {
 	//Calcule la hauteur de la ligne
+	
 	$nb=0;
 	for($i=0;$i<count($data);$i++)
 		$nb=max($nb,$this->NbLines($this->widths[$i],$data[$i]));
@@ -56,21 +59,53 @@ function Row($data)
 		//Dessine le cadre
 		$this->Rect($x,$y,$w,$h);
 		//Imprime le texte
-		$this->MultiCell($w,5,utf8_decode($data[$i]),0,$a);
+		$this->MultiCell($w,5,utf8_decode($data[$i]),1,$a,1);
 		//Repositionne � droite
 		$this->SetXY($x+$w,$y);
 	}
 	//Va � la ligne
 	$this->Ln($h);
 }
-
+function Row($data)
+{
+	//Calcule la hauteur de la ligne
+	
+	$nb=0;
+	for($i=0;$i<count($data);$i++)
+		$nb=max($nb,$this->NbLines($this->widths[$i],$data[$i]));
+	$h=5*$nb;
+	//Effectue un saut de page si n�cessaire
+	$this->CheckPageBreak($h);
+	//Dessine les cellules
+	$compteur = 0;
+	for($i=0;$i<count($data);$i++)
+	{
+		if($compteur==0)
+		$this->SetFillColor('144', '146', '150');
+		else
+		$this->SetFillColor('255', '255', '255');
+		$w=$this->widths[$i];
+		$a=isset($this->aligns[$i]) ? $this->aligns[$i] : 'L';
+		//Sauve la position courante
+		$x=$this->GetX();
+		$y=$this->GetY();
+		//Dessine le cadre
+		$this->Rect($x,$y,$w,$h);
+		//Imprime le texte
+		$this->MultiCell($w,5,utf8_decode($data[$i]),1,$a,1);
+		//Repositionne � droite
+		$this->SetXY($x+$w,$y);
+		$compteur = $compteur + 1;
+	}
+	//Va � la ligne
+	$this->Ln($h);
+}
 function CheckPageBreak($h)
 {
 	//Si la hauteur h provoque un d�bordement, saut de page manuel
 	if($this->GetY()+$h>$this->PageBreakTrigger)
 		$this->AddPage($this->CurOrientation);
 }
-
 function NbLines($w,$txt)
 {
 	//Calcule le nombre de lignes qu'occupe un MultiCell de largeur w
