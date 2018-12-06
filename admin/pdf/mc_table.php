b2cf0b64f96cab2432ec61a70c0ae56a205b1028
Aurelien Brunet <32396173+Mem2o@users.noreply.github.com>
2018-12-06 22:22:47 +0100

    Update mc_table.php

--- admin/pdf/mc_table.php
+++ admin/pdf/mc_table.php
@@ -5,24 +5,36 @@ class PDF_MC_Table extends FPDF
 var $widths;
 var $aligns;
 function Header (){
-			$this->SetFont("Arial","b",10);
-			
+			//Defini la police du header
+			$this->SetFont("Arial","",10);
+			//Defini les couleur du headers
 			$this->SetFillColor(0,0,255);
+			//affiche le logo de l'uvsq 
 			$this->Image('uvsq.jpg',162,5,35);
+			//Perme d'afficher et possitioner la date en temps réele
 			$this->Cell(2,10,date("d/m/Y"),0,0);
+			//Saut de ligne
 			$this->Cell(100,10,'',0,1); 
 			$this->Cell(100,10,'',0,1); 
-			$this->Cell(185,10,'Tableau recapitulatif de l\'avis des etudiants',1,1,'C');
+			//On redefini la taille de la police
+			$this->SetFont("Arial","b",13);
+			//On ecrit le titre et on le met en gras et en encadre
+			$this->Cell(185,10,utf8_decode('Tableau récapitulatif de l\'avis des étudiants'),1,1,'C');
 			$this->Cell(100,10,'',0,1); 
 		}
 		
 function Footer (){
+			//Defnie la police et la positions
+			
 			$this->SetFont("Arial","b",10);
 			$this->SetY(270);
 			$fill = 1;
 			
+			
+			//Permet dafficher le numeros de table en focntions de la pages et le nombre de feuille totalq
 			$this->Cell(0,10,'Page '.$this->PageNo().'/{nb}','B',0,'C');
-			$this->SetFont("Times","b",7);
+			//On ecrit nos noms et on définis la police 
+			$this->SetFont("Arial","",7);
 			$this->Write(10, 'Thomas Lacaze, Jibril Zioui, Aurelien Brunet, Steven Kerautret, Noe Meric de Bellefon, Benjamin Ngogo, Projet PHP 2018 ');
 
 			
@@ -38,6 +50,36 @@ function SetAligns($a)
 	//Tableau des alignements de colonnes
 	$this->aligns=$a;
 }
+function Row2($data)
+{
+	//Calcule la hauteur de la ligne
+	
+	$nb=0;
+	for($i=0;$i<count($data);$i++)
+		$nb=max($nb,$this->NbLines($this->widths[$i],$data[$i]));
+	$h=5*$nb;
+	//Effectue un saut de page si n�cessaire
+	$this->CheckPageBreak($h);
+	//Dessine les cellules
+	for($i=0;$i<count($data);$i++)
+	{
+		
+		$this->SetFillColor('144', '146', '150');
+		$w=$this->widths[$i];
+		$a=isset($this->aligns[$i]) ? $this->aligns[$i] : 'C';
+		//Sauve la position courante
+		$x=$this->GetX();
+		$y=$this->GetY();
+		//Dessine le cadre
+		$this->Rect($x,$y,$w,$h);
+		//Imprime le texte
+		$this->MultiCell($w,5,utf8_decode($data[$i]),1,$a,1);
+		//Repositionne � droite
+		$this->SetXY($x+$w,$y);
+	}
+	//Va � la ligne
+	$this->Ln($h);
+}
 function Row($data)
 {
 	//Calcule la hauteur de la ligne
@@ -49,31 +91,19 @@ function Row($data)
 	//Effectue un saut de page si n�cessaire
 	$this->CheckPageBreak($h);
 	//Dessine les cellules
-	$compteur1 = 0;
+	$compteur = 0;
 	for($i=0;$i<count($data);$i++)
 	{
-		
-		if ($compteur1 == 0)
-		{
-		//$this->SetFillColor('239', '125', '28'); 
-		$w=$this->widths[$i];
-		$a=isset($this->aligns[$i]) ? $this->aligns[$i] : 'L';
-		//Sauve la position courante
-		$x=$this->GetX();
-		$y=$this->GetY();
-		//Dessine le cadre
-		$this->Rect($x,$y,$w,$h);
-		//Imprime le texte
-		$this->MultiCell($w,5,utf8_decode($data[$i]),1,$a,1);
-		//Repositionne � droite
-		$this->SetXY($x+$w,$y);
-		$compteur1 +=1;
-		}
+		if($compteur==0)
+		//ici sont instanciers les cases de couleurs gris : ces a dire les cases exterieurs
+		$this->SetFillColor('144', '146', '150');
 		else
-		{
-		//$this->SetFillColor('255', '255', '255'); 
+		//ici sont instanciers les cases de couleurs blanches : ces a dire les cases intérieurs
+		$this->SetFillColor('255', '255', '255');
+		
+		//On aligne les case du tableau
 		$w=$this->widths[$i];
-		$a=isset($this->aligns[$i]) ? $this->aligns[$i] : 'L';
+		$a=isset($this->aligns[$i]) ? $this->aligns[$i] : 'C';
 		//Sauve la position courante
 		$x=$this->GetX();
 		$y=$this->GetY();
@@ -81,11 +111,9 @@ function Row($data)
 		$this->Rect($x,$y,$w,$h);
 		//Imprime le texte
 		$this->MultiCell($w,5,utf8_decode($data[$i]),1,$a,1);
-		
 		//Repositionne � droite
 		$this->SetXY($x+$w,$y);
-		$compteur1 +=1;
-		}
+		$compteur = $compteur + 1;
 	}
 	//Va � la ligne
 	$this->Ln($h);
