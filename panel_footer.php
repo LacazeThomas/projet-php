8a5c1c1fdf3766efd6ee6eea9627ae1ccae9639c
Thomas LACAZE <contact@thomaslacaze.fr>
2018-12-06 11:03:46 +0100

    Modification footer

--- panel_footer.php
+++ panel_footer.php
@@ -2,7 +2,8 @@
 
 <footer class="footer">
 <div class="container">
-<span class="text-muted">Projet PHP 7/12/18 - Lacaze Thomas, Noé Bellefon, Steven Kerautret, Aurélien Brunet, Jibril Zioui, Benjamin Gogo</span>
+<span class="text-muted"><a href="https://github.com/LacazeThomas/projet-php">Projet PHP 7/12/18</a> - Lacaze Thomas, Noé Bellefon, Steven Kerautret, Aurélien Brunet, Jibril Zioui, Benjamin Gogo</span>
+
 <img src="/g9/assets/img/uvsq.png" class="rounded float-right" style="width:110px;height:55px;" alt= "Logo de Velizy">
 </div>
 </footer>
